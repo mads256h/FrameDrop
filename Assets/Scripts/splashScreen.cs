2f502bc57d1c50278f8efa86d38c9d1f6bcd69be
Mads Mogensen <lort256c@gmail.com>
2015-10-27 15:49:33 +0100

    Revert "Merge pull request #1 from mads256c/Mads"
    This reverts commit caf66b642dbc90ce5d3f3cf9af62a06a405672f6, reversing
    changes made to eebcae0d8b80c53234e543ca57e8d14e840971e8.
    
    Fucking Shit being annoying

--- Assets/Scripts/splashScreen.cs
+++ Assets/Scripts/splashScreen.cs
@@ -3,18 +3,13 @@ using System.Collections;
 
 public class splashScreen : MonoBehaviour {
 
-    public int secoundsToLevelChange = 2;
-    // Use this for initialization
-    void Start()
-    {
-        StartCoroutine(LoadLevel());
-    }
-
-    IEnumerator LoadLevel()
-    {
-        
-        yield return new WaitForSeconds(secoundsToLevelChange);
-        Application.LoadLevel("MainMenu");
-    }
-
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
 }
