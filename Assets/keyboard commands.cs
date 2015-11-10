f762697039dd2de73559187cb2f31b989027fe3b
Petter <uykh@outlook.com>
2015-11-10 16:10:00 +0100

    renamed shit

--- Assets/keyboard commands.cs
+++ Assets/keyboard commands.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class quit : MonoBehaviour {
+public class keyboardcommands : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
