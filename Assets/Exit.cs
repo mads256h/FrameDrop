076ede70bb59ee2e51ef060a873455d2dad2dfc5
Petter <peterfo@live.dk>
2015-11-12 21:17:32 +0100

    changed score aswell as added framelimiter

---
+++ Assets/Exit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Application.Quit();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Application.Quit();
+    }
+}
