92a41900e6d8c551619f2ef637f954593ea23360
Mads Mogensen <lort256c@gmail.com>
2015-11-15 00:43:20 -0800

    Added so many things its insane

--- Assets/Scripts/tutarolManager.cs
+++ Assets/Scripts/tutarolManager.cs
@@ -1,15 +1,42 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class tutarolManager : MonoBehaviour {
 
+    public Canvas canvas;
+    public Text pressToStart;
+
+    bool pressToStartActivated = false;
+    bool enableControls = false;
+
 	// Use this for initialization
 	void Start () {
+        Invoke("EnablePressToStart", 12);
 	
 	}
 	
+
+
 	// Update is called once per frame
 	void Update () {
+
+        if (Input.GetKey(KeyCode.Space) && pressToStartActivated)
+        {
+            canvas.enabled = false;
+            canvas.gameObject.SetActive(false);
+
+            enableControls = true;
+        }
 	
 	}
+
+
+    void EnablePressToStart ()
+    {
+        pressToStart.enabled = true;
+        pressToStart.gameObject.SetActive(true);
+        pressToStartActivated = true;
+
+    }
 }
