65084113c893200e41e2aa31ad9ad595cc1ac859
Mads Mogensen <lort256c@gmail.com>
2015-11-14 17:51:12 -0800

    Removed some useless variables

--- Assets/Scripts/levelGenerator.cs
+++ Assets/Scripts/levelGenerator.cs
@@ -5,20 +5,13 @@ public class levelGenerator : MonoBehaviour {
 
     public Transform[] prefabs;
     public float timesPerSecound = 30;
-    public int currentX = 0;
+    public int currentX = 5;
     public int NumberOfPieces = 500;
-    public int currentPieces = 0;
-    int prefabLength = 0;
-    int rndValue;
+     int currentPieces = 0;
     float timesSecoundFixed = 0;
 
     // Use this for initialization
-    void Start () {
-        prefabLength = prefabs.Length;
-        currentX = 5;
-        
-	
-	}
+
 	
 	// Update is called once per frame
 	void FixedUpdate () {
@@ -26,9 +19,8 @@ public class levelGenerator : MonoBehaviour {
         if (!(currentPieces == NumberOfPieces) && timesSecoundFixed >= 60)
         {
             currentPieces += 1;
-            rndValue = Random.Range(0, prefabLength);
 
-            Instantiate(prefabs[rndValue], new Vector3(currentX, 0, 0), Quaternion.identity);
+            Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(currentX, 0, 0), Quaternion.identity);
             currentX += 5;
 
             timesSecoundFixed = 0;
