0756437e26eb2fa49ccc5ca2633480d6fe21fec6
Mads Mogensen <lort256c@gmail.com>
2015-11-14 17:39:46 -0800

    Fixed some bugs

--- Assets/Scripts/levelGenerator.cs
+++ Assets/Scripts/levelGenerator.cs
@@ -4,13 +4,13 @@ using System.Collections;
 public class levelGenerator : MonoBehaviour {
 
     public Transform[] prefabs;
-    public int timesPerSecound = 30;
+    public float timesPerSecound = 30;
     public int currentX = 0;
     public int NumberOfPieces = 500;
     public int currentPieces = 0;
     int prefabLength = 0;
     int rndValue;
-    float timesSecoundFixed;
+    float timesSecoundFixed = 0;
 
     // Use this for initialization
     void Start () {
@@ -22,15 +22,16 @@ public class levelGenerator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        timesSecoundFixed = (timesPerSecound / 60);
-
-        if (!(currentPieces == NumberOfPieces) && timesPerSecound >= 60)
+        timesSecoundFixed += (timesPerSecound);
+        if (!(currentPieces == NumberOfPieces) && timesSecoundFixed >= 60)
         {
             currentPieces += 1;
             rndValue = Random.Range(0, prefabLength);
 
             Instantiate(prefabs[rndValue], new Vector3(currentX, 0, 0), Quaternion.identity);
             currentX += 5;
+
+            timesSecoundFixed = 0;
 
 
         }
