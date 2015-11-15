99cbc9d7d7b9478a4643e6810e2620b0bd16cf17
Mads Mogensen <lort256c@gmail.com>
2015-11-14 17:28:50 -0800

    Limited levelGenerator
    Limited the amount of times the level generator would spawn platforms
    each secound (Currently 30)
    
    You can change the amount in inspector in unity on the scripts
    gameobject

--- Assets/Scripts/levelGenerator.cs
+++ Assets/Scripts/levelGenerator.cs
@@ -4,11 +4,13 @@ using System.Collections;
 public class levelGenerator : MonoBehaviour {
 
     public Transform[] prefabs;
+    public int timesPerSecound = 30;
     public int currentX = 0;
     public int NumberOfPieces = 500;
     public int currentPieces = 0;
     int prefabLength = 0;
     int rndValue;
+    float timesSecoundFixed;
 
     // Use this for initialization
     void Start () {
@@ -20,8 +22,9 @@ public class levelGenerator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+        timesSecoundFixed = (timesPerSecound / 60);
 
-        if (!(currentPieces == NumberOfPieces))
+        if (!(currentPieces == NumberOfPieces) && timesPerSecound >= 60)
         {
             currentPieces += 1;
             rndValue = Random.Range(0, prefabLength);
