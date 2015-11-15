9d4faa5f60de2335915494361341a15f538ba6de
Mads Mogensen <lort256c@gmail.com>
2015-11-14 23:14:17 -0800

    Added some blood splatter from character

--- Assets/Scripts/dieScript.cs
+++ Assets/Scripts/dieScript.cs
@@ -4,22 +4,28 @@ using System.Collections;
 public class dieScript : MonoBehaviour {
 
     public Transform player;
+    public ParticleSystem particle;
     public Animator anim;
     public GameObject panel;
     public static bool isActivated = false;
     public static bool Activate = false;
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+    // Update is called once per frame
+    void FixedUpdate() {
         if (player.position.y <= -3 && !isActivated || Activate)
         {
-            panel.SetActive(true);
-            anim.SetTrigger("Die");
             isActivated = true;
             Activate = false;
-            Time.timeScale = 0;
-            
-            
+            particle.enableEmission = true;
+            Invoke("Die", 5);
+
         }
     }
-}
+
+    void Die ()
+      {
+        panel.SetActive(true);
+        anim.SetTrigger("Die");
+        Time.timeScale = 0;
+    }
+    }
