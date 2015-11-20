ff93d76899214aeea50e618163e843d77aaa1dd1
mads256c <lort256c@gmail.com>
2015-11-20 11:01:16 +0100

    Added comments in dieScript

--- Assets/Scripts/dieScript.cs
+++ Assets/Scripts/dieScript.cs
@@ -10,14 +10,25 @@ public class dieScript : MonoBehaviour {
     public static bool isActivated = false;
     public static bool Activate = false;
 
-    // Update is called once per frame
+    // Fixed update is called 60 times every secound
     void FixedUpdate() {
+
+		// if the players position is under -3 and is not activated or activate is true
         if (player.position.y <= -3 && !isActivated || Activate)
-        {
+		{
+			// Enable the panel
             panel.SetActive(true);
+
+			// Set the trigger Die
             anim.SetTrigger("Die");
+
+			// Set isActivated to true to not execute this program over and over again
             isActivated = true;
+
+			// Set Activate to false
             Activate = false;
+
+			// Set the timeScale to 0 to stop execution of scripts and stop the score from counting up
             Time.timeScale = 0;
 
         }
