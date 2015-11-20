1871806351f96077d1b85b07eb06c65c2cab6ccd
Mads Mogensen <lort256c@gmail.com>
2015-11-19 21:56:13 -0800

    Added option for musicManager and put music in tutoral

--- Assets/Scripts/musicManager.cs
+++ Assets/Scripts/musicManager.cs
@@ -5,17 +5,40 @@ public class musicManager : MonoBehaviour {
     public AudioSource audioSource;
     public AudioClip[] musicClips;
 
+    public bool playNext = false;
+    int currentSong = 0;
+
 
 	// Use this for initialization
 	void Start () {
-        playNextSong();
+        if (playNext)
+        {
+            playNextSong();
+        }
+        else
+        {
+            playRandomSong();
+        }
 	}
 	
-	// Play next song
-    void playNextSong()
+	// Play Random song
+    void playRandomSong()
     {
         audioSource.clip = musicClips[Random.Range(0, musicClips.Length)];
         audioSource.Play();
+        Invoke("playRandomSong", audioSource.clip.length);
+    }
+
+    void playNextSong()
+    {
+        audioSource.clip = musicClips[currentSong];
+        audioSource.Play();
+        currentSong += 1;
+        if (currentSong == musicClips.Length + 1)
+        {
+            currentSong = 0;
+        }
         Invoke("playNextSong", audioSource.clip.length);
+
     }
 }
