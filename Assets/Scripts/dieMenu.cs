6dcc320ffc1214aaeb66d2c6d0d42cddc85f6ea2
mads256c <lort256c@gmail.com>
2015-11-20 10:57:16 +0100

    Added comments to dieMenu.cs

--- Assets/Scripts/dieMenu.cs
+++ Assets/Scripts/dieMenu.cs
@@ -3,16 +3,27 @@ using System.Collections;
 
 public class dieMenu : MonoBehaviour {
     public GameObject panel;
+
+	// Restart level function
     public void restartLevel()
     {
+		// Set the timescale to the normal timescale
         Time.timeScale = 1;
+
+		// Load the same level we already is in
         Application.LoadLevel(Application.loadedLevel);
+
+		// Disable the panel
         panel.SetActive(false);
+
+		// Disable die in dieScript (We are not dying right now)
         dieScript.isActivated = false;
     }
 
+	// Exit game function
 public void exitGame()
     {
+		// Exit the application
         Application.Quit();
     }
 }
