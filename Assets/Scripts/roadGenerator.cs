7f5f0f2a6d7caf2aa77d195346838191ed7ca28c
mads256c <lort256c@gmail.com>
2015-11-20 11:33:31 +0100

    RoadGenerator.cs Comments

--- Assets/Scripts/roadGenerator.cs
+++ Assets/Scripts/roadGenerator.cs
@@ -9,17 +9,25 @@ public class roadGenerator : MonoBehaviour {
     int currentPieces = 0;
     float timesSecoundFixed = 0;
 
-    // Update is called once per frame
+    // Fixed update is called 60 times per secound
     void FixedUpdate()
     {
+		// add timesPerSecound to timesSecoundFixed
         timesSecoundFixed += (timesPerSecound);
+
+		// if currentPieces is not NumberOfPieces and timesSecoudFixed is equal or greater than 60
         if (!(currentPieces == levelGenerator.NumberOfPieces) && timesSecoundFixed >= 60)
         {
+			// Add 1 to currentPieces
             currentPieces += 1;
 
+			// Spawn the prefab
             Instantiate(prefab, new Vector3(currentX, -5, 0), Quaternion.identity);
+
+			// Add 5 to currentX
             currentX += 5;
 
+			// Resey timesSecoundFixed
             timesSecoundFixed = 0;
         }
 
