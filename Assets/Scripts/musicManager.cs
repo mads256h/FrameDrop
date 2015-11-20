c22e63325af57dcacce47518120f03473367be1a
mads256c <lort256c@gmail.com>
2015-11-20 11:15:37 +0100

    musicManager comments

--- Assets/Scripts/musicManager.cs
+++ Assets/Scripts/musicManager.cs
@@ -11,12 +11,16 @@ public class musicManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+		// if bool playNext is true
         if (playNext)
         {
+			//Play nextsong
             playNextSong();
         }
         else
         {
+			//Instead of random song
             playRandomSong();
         }
 	}
@@ -24,20 +28,36 @@ public class musicManager : MonoBehaviour {
 	// Play Random song
     void playRandomSong()
     {
+		// Set the audioSource.clip to a random one
         audioSource.clip = musicClips[Random.Range(0, musicClips.Length)];
+
+		// Play the song
         audioSource.Play();
+
+		// Invoke the function again after the song has ended
         Invoke("playRandomSong", audioSource.clip.length);
     }
 
+	// Play Next song
     void playNextSong()
     {
+		// Set the clip to current song
         audioSource.clip = musicClips[currentSong];
+
+		// Play the clip
         audioSource.Play();
+
+		// Add 1 to currentSong
         currentSong += 1;
+
+		// if currentSong is musicClips.Length + 1
         if (currentSong == musicClips.Length + 1)
         {
+			// Set it to 0
             currentSong = 0;
         }
+
+		// Invoke the same function after the clip has ended
         Invoke("playNextSong", audioSource.clip.length);
 
     }
