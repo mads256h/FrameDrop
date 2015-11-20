1630f6bc99c45aa10bee8ef6576633d03353c03b
mads256c <lort256c@gmail.com>
2015-11-20 13:00:55 +0100

    Added more comments

--- Assets/Scripts/tutarolManager.cs
+++ Assets/Scripts/tutarolManager.cs
@@ -14,6 +14,8 @@ public class tutarolManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+		// Invoke Enable Press To Start after 12 secounds
         Invoke("EnablePressToStart", 12);
 	
 	}
@@ -23,23 +25,32 @@ public class tutarolManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		// Check for space or Enter and bool pressToStartActivated
         if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Return)) && pressToStartActivated)
         {
+			// Disable welcomeCanvas
             welcomeCanvas.enabled = false;
             welcomeCanvas.gameObject.SetActive(false);
+
+			// Disable Press To Start
             pressToStart.enabled = false;
             pressToStart.gameObject.SetActive(false);
 
+			// Enable the player
             player.SetActive(true);
+
         }
 	
 	}
 
-
+	// Enable Press To Start Function
     void EnablePressToStart ()
     {
+		// Enable Press To Start
         pressToStart.enabled = true;
         pressToStart.gameObject.SetActive(true);
+
+		// Set pressToStartActivated to true
         pressToStartActivated = true;
 
     }
