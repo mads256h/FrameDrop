572a6a01deba6fa46b8b152c65fba7285dc09186
Mads Mogensen <lort256c@gmail.com>
2015-11-28 14:34:06 -0800

    Added a settings menu and added scripts to it

---
+++ Assets/Scripts/settingsManager.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class settingsManager : MonoBehaviour {
+
+    public Camera cam;
+
+    public Canvas settingsCanvas;
+    public Canvas mainMenuCanvas;
+    public Slider fovSlider;
+    public Toggle lagSpikeToggle;
+    public Text lagspikeToggleText;
+    public Toggle frameDropToggle;
+    public Text frameDropToggleText;
+
+    public float getFov = 0.0f;
+
+    public static float fov = 5.0f;
+    public static bool lagSpikeEnabled = true;
+    public static bool frameDropEnabled = true;
+
+    bool isMenu = false;
+    bool isGame = false;
+
+	// Use this for initialization
+	void Start () {
+	if (cam == null)
+        {
+            isMenu = true;
+            isGame = false;
+        }
+    if (fovSlider == null)
+        {
+            isGame = true;
+            isMenu = false;
+            cam.orthographicSize = fov;
+        }
+    if (isMenu)
+        {
+            lagSpikeToggle.isOn = lagSpikeEnabled;
+            frameDropToggle.isOn = frameDropEnabled;
+            settingsCanvas.enabled = false;
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isMenu && settingsCanvas.enabled)
+        {
+            fov = fovSlider.value;
+            lagSpikeEnabled = lagSpikeToggle.isOn;
+            frameDropEnabled = frameDropToggle.isOn;
+            
+
+            if (lagSpikeToggle.isOn)
+            {
+                lagspikeToggleText.text = "Enabled";
+            }
+            else
+            {
+                lagspikeToggleText.text = "Disabled";
+            }
+
+            if (frameDropToggle.isOn)
+            {
+                frameDropToggleText.text = "Enabled";
+            }
+            else
+            {
+                frameDropToggleText.text = "Disabled";
+            }
+        }
+        getFov = fov;
+	}
+
+    public void EnableSettings()
+    {
+        settingsCanvas.enabled = true;
+        mainMenuCanvas.enabled = false;
+        Debug.Log("Trying to upen settings");
+    }
+
+    public void DisableSettings()
+    {
+        settingsCanvas.enabled = false;
+        mainMenuCanvas.enabled = true;
+    }
+}
