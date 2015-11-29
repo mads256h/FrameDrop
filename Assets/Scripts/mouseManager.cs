176c4e187743c1a30cf61147fcb3b181f0318db3
Mads Mogensen <lort256c@gmail.com>
2015-11-28 17:57:58 -0800

    New cursor, it works and with a build

--- Assets/Scripts/mouseManager.cs
+++ Assets/Scripts/mouseManager.cs
@@ -2,49 +2,57 @@
 using System.Collections;
 
 public class mouseManager : MonoBehaviour {
-    public SpriteRenderer cursor;
+
+
+    public Transform cursor;
+    public SpriteRenderer cursorSprite;
+
     public Sprite[] cursors;
     public Sprite blank;
-    public static string mouseState = "None";
-	// Use this for initialization
-	void Start () {
-        Cursor.visible = false;
-	
-	}
-	
-	// Update is called once per frame
+    public static string mouseState = "none";
+
+
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(new Vector3(mousePos.x, mousePos.y, -2));
+
+        cursor.position = objectPos;
+    }
+	// FixedUpdate is called 60 times per secound
 	void FixedUpdate () {
 
         switch (mouseState.ToLower())
         {
             case "system":
                 Cursor.visible = true;
-                cursor.sprite = blank;
+                cursorSprite.sprite = blank;
                 break;
 
             case "none":
                 Cursor.visible = false;
-                cursor.sprite = cursors[0];
+                cursorSprite.sprite = cursors[0];
+                Debug.Log("Now using none");
                 break;
 
             case "cursor":
                 Cursor.visible = false;
-                cursor.sprite = cursors[1];
+                cursorSprite.sprite = cursors[1];
                 break;
 
             case "pistol":
                 Cursor.visible = false;
-                cursor.sprite = cursors[2];
+                cursorSprite.sprite = cursors[2];
                 break;
 
             case "machinegun":
                 Cursor.visible = false;
-                cursor.sprite = cursors[3];
+                cursorSprite.sprite = cursors[3];
                 break;
 
             default:
                 Cursor.visible = false;
-                cursor.sprite = cursors[0];
+                cursorSprite.sprite = cursors[0];
                 break;
         }
 	
