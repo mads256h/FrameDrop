2d3280eddc5feeb4a1c3071827f12ab10e4e22a1
Mads Mogensen <lort256c@gmail.com>
2015-11-28 16:41:58 -0800

    Added support for custom cursors

---
+++ Assets/Scripts/mouseManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouseManager : MonoBehaviour {
+    public SpriteRenderer cursor;
+    public Sprite[] cursors;
+    public Sprite blank;
+    public static string mouseState = "None";
+	// Use this for initialization
+	void Start () {
+        Cursor.visible = false;
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        switch (mouseState.ToLower())
+        {
+            case "system":
+                Cursor.visible = true;
+                cursor.sprite = blank;
+                break;
+
+            case "none":
+                Cursor.visible = false;
+                cursor.sprite = cursors[0];
+                break;
+
+            case "cursor":
+                Cursor.visible = false;
+                cursor.sprite = cursors[1];
+                break;
+
+            case "pistol":
+                Cursor.visible = false;
+                cursor.sprite = cursors[2];
+                break;
+
+            case "machinegun":
+                Cursor.visible = false;
+                cursor.sprite = cursors[3];
+                break;
+
+            default:
+                Cursor.visible = false;
+                cursor.sprite = cursors[0];
+                break;
+        }
+	
+	}
+}
