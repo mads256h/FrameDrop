bfa4cb6f9f1887b09b9126183b88ba1627a9d105
Mads Mogensen <lort256c@gmail.com>
2015-12-01 13:37:43 -0800

    Added more functionality to enemyController

--- Assets/Scripts/enemyController.cs
+++ Assets/Scripts/enemyController.cs
@@ -3,16 +3,19 @@ using System.Collections;
 
 public class enemyController : MonoBehaviour {
 
-    public Rigidbody enemy;
     public int speed = 100;
     public int startingHealth = 100;
 
+    Rigidbody rigid;
     int Health = 0;
     bool right = false;
     Vector3 movement;
     
-	
-    
+	void Start()
+    {
+        rigid = this.gameObject.GetComponent<Rigidbody>();
+    }
+
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "SwitchDir")
@@ -30,7 +33,7 @@ public class enemyController : MonoBehaviour {
     
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
         if (right)
         {
             Move(1);
@@ -44,8 +47,10 @@ public class enemyController : MonoBehaviour {
 
     void Move(float h)
     {
+        
+
         // if the player is moving under the max speed or going backwards
-        if (enemy.velocity.magnitude < speed / 100)
+        if (rigid.velocity.magnitude < speed / 100)
         {
 
             // Set the movement vector based on the axis input.
@@ -54,8 +59,9 @@ public class enemyController : MonoBehaviour {
             // Normalise the movement vector and make it proportional to the speed per second.
             movement = movement.normalized * speed * Time.deltaTime;
 
+            
             // Move the player to it's current position plus the movement.
-            enemy.AddForce(transform.right * h * speed * Time.deltaTime);
+            rigid.AddForce(transform.right * h * speed * Time.deltaTime);
         }
     }
 
