46cebaf6cb22816b821b276df01075d58076de7d
Mads Mogensen <lort256c@gmail.com>
2015-12-02 13:11:37 -0800

    Starting to add a hud over the courser
    It's gonna be nice :+1:

---
+++ Assets/Scripts/courserAmmoManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class courserAmmoManager : MonoBehaviour {
+
+    public SpriteRenderer courserAmmoObject;
+    public Sprite[] gunAmmoSprites;
+
+
+	// Update is called once per frame
+	void FixedUpdate () {
+	if (gunManager.ActiveGun != -1)
+        {
+            courserAmmoObject.sprite = gunAmmoSprites[gunManager.ActiveGun];
+        }
+	}
+}
