3f7bd23d1d6b5836a6711fe00b2e68c2cf96b83a
DaaseAllan <laurits.mumberg@gmail.com>
2015-12-08 16:42:01 +0100

    Good shit right there

--- Assets/Animationer/Anim.cs
+++ Assets/Animationer/Anim.cs
@@ -16,6 +16,7 @@ public class Anim : MonoBehaviour {
 		float move = Input.GetAxis ("Horizontal");
 		anim.SetFloat("Speed", move);
 		
-
+		bool Jump = Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || Input.GetAxisRaw("Jump") >= 0.75;
+		anim.SetBool ("Jump", Jump);
 	}
 }
