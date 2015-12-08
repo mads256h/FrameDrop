472ce82d70807f5fd2b74fa7f6e485813b107666
DaaseAllan <laurits.mumberg@gmail.com>
2015-12-08 15:45:34 +0100

    DU. KAN. NU. GÅ. FREMAAAAAAD!!D!!D!D!D!!1111!!!et!!!!
    rad

---
+++ Assets/Animationer/Anim.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Anim : MonoBehaviour {
+	Animator anim;
+	
+	
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+	}
+	
+	
+	void Update ()
+	{
+		float move = Input.GetAxis ("Horizontal");
+		anim.SetFloat("Speed", move);
+		
+
+	}
+}
